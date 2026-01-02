3dddd67c357c50dde26df441f10a96899358128a
Duhan Mo <159794032+DuhanMo@users.noreply.github.com>
2026-01-02 18:06:53 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 29.12 ms, Memory: 67.2 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.kt
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.kt
@@ -1,53 +1,36 @@
+import kotlin.math.abs
+
 class Solution {
     fun solution(survey: Array<String>, choices: IntArray): String {
-        val scoreMap = mutableMapOf(
-            'R' to 0,
-            'T' to 0,
-            'C' to 0,
-            'F' to 0,
-            'J' to 0,
-            'M' to 0,
-            'A' to 0,
-            'N' to 0,
-        )
+        val scoreMap = mutableMapOf<Char, Int>().withDefault { 0 }
+        // 순회
+        // 점수 4를 기준으로 스코어맵에 점수 더하기
+        // 리스트 쌍을 만들어서 그 값을 비교하고 결과를 joinToString
         for (i in survey.indices) {
+            // AC, TC,..
             val pair = survey[i]
-
-            val first = pair[0]
-            val second = pair[1]
-            when (choices[i]) {
-                1 -> scoreMap[first] = scoreMap[first]!! + 3
-                2 -> scoreMap[first] = scoreMap[first]!! + 2
-                3 -> scoreMap[first] = scoreMap[first]!! + 1
-                
-                5 -> scoreMap[second] = scoreMap[second]!! + 1
-                6 -> scoreMap[second] = scoreMap[second]!! + 2
-                7 -> scoreMap[second] = scoreMap[second]!! + 3
+            // 1,2,3,4,5,6,7
+            // choice - 4 = -3, -2,-1,0,1,2,3
+            val score = choices[i] - 4
+            if (score < 0) {
+                scoreMap[pair[0]] = (scoreMap[pair[0]]?:0) + abs(score)
+            } else if (score > 0) {
+                scoreMap[pair[1]] = (scoreMap[pair[1]]?:0) + score
             }
         }
         
-        val sb = StringBuilder()
-        when {
-            scoreMap['R']!! > scoreMap['T']!!  -> sb.append("R")
-            scoreMap['R']!! < scoreMap['T']!!  -> sb.append("T")
-            else  -> sb.append("R")
-        }
-        when {
-            scoreMap['C']!! > scoreMap['F']!!  -> sb.append("C")
-            scoreMap['C']!! < scoreMap['F']!!  -> sb.append("F")
-            else  -> sb.append("C")
-        }
-        when {
-            scoreMap['J']!! > scoreMap['M']!!  -> sb.append("J")
-            scoreMap['J']!! < scoreMap['M']!!  -> sb.append("M")
-            else  -> sb.append("J")
-        }
-        when {
-            scoreMap['A']!! > scoreMap['N']!!  -> sb.append("A")
-            scoreMap['A']!! < scoreMap['N']!!  -> sb.append("N")
-            else  -> sb.append("A")
-        }
-        
-        return sb.toString()
+        val types = listOf("RT", "CF", "JM", "AN")
+        return types.map { type ->
+            // "R"
+            val first = type[0]
+            // "T"
+            val second = type[1]
+            
+            if ((scoreMap[first] ?: 0) >= (scoreMap[second] ?: 0)) {
+                first
+            } else {
+                second
+            }
+        }.joinToString("")
     }
 }
